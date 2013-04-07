6c7ee4cc0602c80b44d2114df43807310fcda943
Sergey Chipiga <sergey.chipiga@flatstack.com>
2013-04-07 11:09:14 +0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsBlock::Application.config.secret_token = '87ebd4d6490e7017e4d7379306019be527d3ac5a697dd72737852fcbbae86fd9a57b3f9776a3fc9a3327997e574a6d03664e2e826608d7c6f99d748b89f2f3ce'
